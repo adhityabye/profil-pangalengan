15c77d95f254500ac0f05c626856f8c9d4b23b0e
adhityabye <mr.adhityabayu@gmail.com>
2024-07-29 22:45:35 +0700

    style: add images assets

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -12,7 +12,24 @@ module.exports = {
         josefin: ["Josefin Sans", "sans"],
       },
       colors: {
-        "main-color": "#F3F3F3",
+        "white-100": "#FBFBFB",
+        "black-800": "#282828",
+        "black-900": "#2E2E2E",
+        "gray-100": "#EAEAEA",
+        "gray-200": "#B1B1B1",
+        "gray-300": "#757575",
+        "gray-400": "#D4D4D4",
+        "gray-500": "#5C5C5C",
+        "purple-100": "rgba(86, 75, 188, 0.14)",
+        "purple-200": "#564BBC",
+        "purple-900": "#202B51",
+        "gray-input": "#EAEAEA",
+        "button-primary": "#478CCF",
+        "button-primary-hover": "#92c2f0",
+        "button-dark": "#202B51",
+        main: "#F3F3F3",
+        secondary: "#62C581",
+        orange: "#FFA500",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
